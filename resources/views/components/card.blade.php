dab2f6f6f47512ced0d7e8eb80988b2cfaf91853
Heng David <hengdavid129@gmail.com>
2026-08-06 15:56:45 +0700

    Show A Single Idea

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,9 @@
-<a {{ $attributes(['class' => 'border border-border rounded-lg bg-card p-4 md:text-sm']) }} >
-    {{ $slot }}
-</a>
+@if ($attributes->has('href'))
+    <a {{ $attributes(['class' => 'border border-border rounded-lg bg-card p-4 md:text-sm']) }} >
+        {{ $slot }}
+    </a>
+@else
+    <div {{ $attributes(['class' => 'border border-border rounded-lg bg-card p-4 md:text-sm']) }}>
+        {{ $slot }}
+    </div>
+@endif
